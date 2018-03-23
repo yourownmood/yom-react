e7c00c247b1390b834d79577df439cf0181b32ca
Steven Bax <sbax@yourownmood.nl>
2018-03-23 16:06:07 +0100

    Adds scss processing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 //
-import './app.css'
+import './App.css'
+import './styles/main.css'
 import logo from './logo.png'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React-Static</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React-Static</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
---
+++ src/styles/tools/_tools.aliases.spec.js
@@ -0,0 +1,34 @@
+/* global describe it */
+
+const path = require('path')
+const Sassaby = require('sassaby')
+
+describe('_tools.aliases.scss', function () {
+  const file = path.resolve(__dirname, '_tools.aliases.scss')
+  const sassaby = new Sassaby(file, {
+    variables: {
+      'inuit-global-baseline': '6px',
+      'inuit-global-spacing-unit': '12px',
+      'inuit-global-spacing-unit-tiny': '8px',
+      'inuit-global-spacing-unit-small': '4px',
+      'inuit-global-spacing-unit-large': '20px',
+      'inuit-global-spacing-unit-huge': '40px',
+      'inuit-global-font-size': '16px',
+      'inuit-global-line-height': '1em'
+    },
+    dependencies: [
+      path.resolve(__dirname, '_tools.font-size.scss'),
+      path.resolve(__dirname, '_tools.clearfix.scss')
+    ]
+  })
+
+  describe('Alias mixins', function () {
+    it('should call inuit-font-size correctly with font-size', function () {
+      sassaby.includedMixin('font-size').calledWithArgs('12px').declares('font-size', '.75rem')
+    })
+
+    it('should call inuit-clearfix correctly with clearfix', function () {
+      sassaby.includedMixin('clearfix').called().declares('display', 'block!important')
+    })
+  })
+})
