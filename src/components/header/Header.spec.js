67365f0d14aff98ab182aa9051d4bd66ce713883
Steven Bax <sbax@yourownmood.nl>
2018-04-22 19:39:30 +0200

    Fixes flow & unit-tests for page-transitions

--- src/components/header/Header.spec.js
+++ src/components/header/Header.spec.js
@@ -2,13 +2,28 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
-import { createMemoryHistory } from 'history'
 
 import { Header } from './Header'
 
+const history = {
+  length: 1,
+  location: {
+    pathname: '/contact',
+    search: '',
+    hash: 'asd123'
+  },
+  action: 'PUSH',
+  listen: jest.fn(),
+  push: jest.fn(),
+  replace: jest.fn(),
+  go: jest.fn(),
+  goBack: jest.fn(),
+  goForward: jest.fn(),
+  block: jest.fn()
+}
+
 describe('Header', () => {
   it('should render without crashing', () => {
-    const history = createMemoryHistory('/home')
     const component = shallow(<Header history={history} />)
     expect(component.find('.header').length).toBe(1)
   })
